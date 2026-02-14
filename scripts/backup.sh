bfe491a95d0b8d1454675fa1adce20b4c2f9841c
umairrahmadd <umairiqrar20@gmail.com>
2026-02-14 16:13:18 +0500

    commit

---
+++ scripts/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Source and destination paths
+src=/home/umair/mydata
+dest=/home/umair/backup
+
+# Local backup (copy folder with proper timestamp)
+mkdir -p $dest
+cp -r $src $dest/backup_$(date +%Y-%m-%d_%H-%M-%S)
+
+# Upload to AWS S3
+aws s3 sync $dest s3://aws-backups-475739/ --delete
+
+# Logging
+echo "Backup folder copied and uploaded to S3 at $(date '+%Y-%m-%d %H:%M:%S')" >> /home/umair/backup.log
